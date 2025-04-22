aa7a8e4a2bb1c52f4cd6d4cca121345f1571ba69
leminhnguyenai <leminhnguyenai@gmail.com>
2025-04-22 13:48:29 +0700

    Add support for colored logger

---
+++ internal/common/logger.go
@@ -0,0 +1,38 @@
+package common
+
+type Style string
+
+var (
+	Reset = "\033[0m"
+
+	RedFg     Style = "\033[38;5;196m"
+	GreenFg   Style = "\033[38;5;46m"
+	YellowFg  Style = "\033[38;5;226m"
+	BlueFg    Style = "\033[38;5;27m"
+	MagentaFg Style = "\033[38;5;201m"
+	CyanFg    Style = "\033[38;5;51m"
+	GrayFg    Style = "\033[38;5;245m"
+	WhiteFg   Style = "\033[38;5;15m"
+
+	RedBg     Style = "\033[48;5;196m"
+	GreenBg   Style = "\033[48;5;46m"
+	YellowBg  Style = "\033[48;5;226m"
+	BlueBg    Style = "\033[48;5;27m"
+	MagentaBg Style = "\033[48;5;201m"
+	CyanBg    Style = "\033[48;5;51m"
+	GrayBg    Style = "\033[48;5;245m"
+	WhiteBg   Style = "\033[48;5;15m"
+
+	Bold      Style = "\033[1m"
+	Underline Style = "\033[4m"
+)
+
+func ColorStringFg(str string, colors ...Style) string {
+	result := str
+
+	for _, color := range colors {
+		result = string(color) + result + Reset
+	}
+
+	return result
+}
--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -3,12 +3,14 @@ package middlewares
 import (
 	"log"
 	"net/http"
+
+	"github.com/leminhnguyenai/personal-blog/internal/common"
 )
 
 func LoggerMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// NOTE: Add color to the logger
-		log.Printf("%s: %s", r.Method, r.URL.Path)
+		log.Printf(common.ColorStringFg("%s:", common.RedFg, common.WhiteBg)+"%s\n", r.Method, r.URL.Path)
 
 		h.ServeHTTP(w, r)
 	})
